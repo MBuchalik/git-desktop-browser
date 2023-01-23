9a384ee51b3550a94f8702aa3f0f78d8944eba3f
MBuchalik <dev@martin-buchalik.de>
2023-01-23 12:09:56 +0100

    Apply changes required by the update to React 18

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './app';
 import './theme/theme.scss';
 
-render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') as HTMLElement,
 );
