8f4910a5f32f2bef83a1da3d32d3c8bfcb59cd47
MBuchalik <dev@martin-buchalik.de>
2023-01-28 11:07:32 +0100

    Distinguish development and production mode builds

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,45 @@
+const electronForgeCore = require('@electron-forge/core');
+
+const BUILD_MODE_FLAG = '--buildMode=';
+
+const buildModeArg = process.argv.find((item) =>
+  item.startsWith(BUILD_MODE_FLAG),
+);
+if (!buildModeArg) {
+  throw Error('You need to provide the "--buildMode" flag.');
+}
+
+const buildMode = buildModeArg.replace(BUILD_MODE_FLAG, '');
+
+const BUILD_MODE_PRODUCTION = 'prod';
+const BUILD_MODE_DEVELOPMENT = 'dev';
+const knownBuildModes = [BUILD_MODE_PRODUCTION, BUILD_MODE_DEVELOPMENT];
+if (!knownBuildModes.includes(buildMode)) {
+  throw Error('Unknown value for the Build Mode flag.');
+}
+
 module.exports = {
+  buildIdentifier: buildMode,
   packagerConfig: {
-    name: 'Git Desktop Browser',
+    name:
+      buildMode === BUILD_MODE_PRODUCTION
+        ? 'Git Desktop Browser'
+        : 'Git Desktop Browser (Dev)',
+    appBundleId: electronForgeCore.utils.fromBuildIdentifier({
+      [BUILD_MODE_PRODUCTION]: 'git-desktop-browser',
+      [BUILD_MODE_DEVELOPMENT]: 'git-desktop-browser-dev',
+    }),
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name:
+          buildMode === BUILD_MODE_PRODUCTION
+            ? 'git-desktop-browser'
+            : 'git-desktop-browser-dev',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
